d4f668fa167057d84ae571a2c18d503f3e2ea467
HarshIT01001000 <raiharshit009@gmail.com>
2025-06-27 21:29:41 +0530

    protectedRoute setup

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,15 @@ import App from './App.jsx'
 import Login from './Components/Login.jsx';
 import Register from './Components/Register.jsx';
 import ChatBox from './Components/ChatBox.jsx';
+import ProtectedRoute from './ProtectedRoute.jsx';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
    <Router>
     <Routes>
       <Route path="/" element={<Login/>}/>
       <Route path="/register" element={<Register />} />
-      <Route path="/Home" element={<App/>}/> 
-      <Route path="/ChatBox" element={<ChatBox/>}/> 
+      <Route path="/Home" element={<ProtectedRoute><App/></ProtectedRoute>}/> 
+      <Route path="/ChatBox" element={<ProtectedRoute><ChatBox/></ProtectedRoute>}/> 
     </Routes>
    </Router>
   </StrictMode>
